58b457a63dce7a919ae47f69c6fe5e4b88204a54
tanglong <dreamlong80@gmail.com>
2019-09-09 17:07:22 +0800

    feat：添加db

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,7 @@ arr=(${IPADDR//./ })
 IPADDR3=${arr[0]}.${arr[1]}.${arr[2]}
 sed -i "s/^ServerName .*$/ServerName ${IPADDR}:81/g" /etc/httpd/conf/httpd.conf
 sed -i "s/^ServerName .*$/ServerName ${IPADDR}:443/g" /etc/httpd/conf.d/ssl.conf
-sed -i "s/^subnet .* netmask /subnet ${IPADDR} netmask /g" /etc/cobbler/dhcp.template
+sed -i "s/^subnet .* netmask /subnet${IPADDR3}.0 netmask /g" /etc/cobbler/dhcp.template
 sed -i "s/^option routers .*$/option routers ${IPADDR};/g" /etc/cobbler/dhcp.template
 sed -i "s/^option domain-name-servers .*$/option domain-name-servers $IPADDR;/g" /etc/cobbler/dhcp.template
 sed -i "s/^range dynamic-bootp .*$/range dynamic-bootp ${IPADDR3}.120 ${IPADDR3}.254;/g" /etc/cobbler/dhcp.template
